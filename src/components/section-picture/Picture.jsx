f7076fc90ac403123eb86be00d5fdb6e8948fc1f
SFERAS <rthyssen73@gmail.com>
2023-03-22 23:26:07 -0300

    text decoration

--- src/components/section-picture/Picture.jsx
+++ src/components/section-picture/Picture.jsx
@@ -9,10 +9,10 @@ export default function Pictuer() {
 				<img src={home} alt="baner" className="onscroll-image-fade-in"/>
 			</div>
 			<div className="moto">
-				<h1>Limpieza integral diaria.</h1>
-				<h1>Eco-limpieza de oficinas.</h1>
+				<h1><mark>Limpieza</mark> integral diaria.</h1>
+				<h1>Eco-limpieza de <mark>oficinas</mark>.</h1>
 				<h1>Limpieza de oficinas después de la renovación.</h1>
-				<h1>Limpieza desinfectante de oficinas.</h1>
+				<h1>Limpieza <mark>desinfectante</mark>  de oficinas.</h1>
 			</div>
 		</div>
 	)
