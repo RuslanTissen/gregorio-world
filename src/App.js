7e567797eff0176bd0c2577228e5a0182414f806
SFERAS <rthyssen73@gmail.com>
2023-04-02 22:53:19 -0300

    first version ready

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Expo from './components/expo/Expo';
 import Footer from './components/footer/Footer';
 import photo2 from "./images/photo-2.png"
 import photo3 from "./images/photo-3.png"
-import photo4 from "./images/photo-4.png"
+// import photo4 from "./images/photo-4.png"
 import personal from "./images/personal.png"
 // import wa from "./images/WhatsAppButtonGreenMedium.png"
 import { BsWhatsapp } from "react-icons/bs"
@@ -21,23 +21,23 @@ function App() {
 			<div className='app-container'>
 				<Pictuer />
 				<Expo/>
-				<div id={'alq'} >
+				{/* <div id={'alq'} >
 					<MidSection
 						image={photo2}
 						// title={"Nuestro lema:"}
 						text={<div style={{ margin: "25px 0" }}>Nuestro personal tiene muchos años de experiencia. Y seguimos mejorando tanto los métodos de limpieza como los medios para mejorar el servicio."</div>}
 					/>
-				</div>
-				<div id={'detalles'} >
+				</div> */}
+				{/* <div id={'detalles'} >
 					<MidSection
 						image={photo3}
 						// title={"Open Time"}
 						text={<div style={{ margin: "25px 0" }}>Utilizamos solo herramientas y equipos profesionales. Productos a partir de materias primas naturales. No causa alergias, seguro para niños y animales.</div>}
 					/>
-				</div>
+				</div> */}
 				<div id={'contacto'}>
 					<MidSection
-						image={photo4}
+						image={photo3}
 						price={<div style={{ margin: "30px" }}>¿Quieres calcular el costo?</div>}
 						a={<a className='btn' href='mailto:rthyssen73@gmail.com'>Contáctame</a>}
 					// a={<a aria-label="Chat on WhatsApp" target="_blank" href="https://wa.me/5491166014163"><BsWhatsapp style={{ margin: "5px 0", width: "2em", height: "2em", color: "rgb(0,168,132)" }} /> </a>} 
--- src/components/section-picture/Picture.jsx
+++ src/components/section-picture/Picture.jsx
@@ -3,19 +3,24 @@ import './Picture.scss'
 import home from "../../images/photo-1.png"
 import { BsFillArrowRightCircleFill } from "react-icons/bs"
 import AnchorLink from 'react-anchor-link-smooth-scroll-v2';
+import { BsArrowDownCircleFill, BsArrowDownCircle } from "react-icons/bs"
 
 export default function Pictuer() {
 	return (
 		<div id="foto" className="picture">
+
 			<div className="picture-container">
-				<img src={home} alt="baner" className="onscroll-image-fade-in" />
-				<button className="move-button"><AnchorLink href="#images"><BsFillArrowRightCircleFill /></AnchorLink></button>
+				<img src={home} alt="baner" />
 			</div>
+
 			<div className="moto">
-				<h1>Bienvenido al mundo de la  <mark>pureza</mark> </h1>
-				{/* <h1><mark></mark></h1>
-				<h1></h1>
-				<h1> <mark></mark>  </h1> */}
+				<div className="text-container">
+					<h1>Bienvenido al mundo de la  <mark>pureza</mark> </h1>
+				</div>
+				<div className="button-container">
+					<AnchorLink href="#images"><BsArrowDownCircleFill /></AnchorLink>
+				</div>
+
 			</div>
 		</div>
 	)
